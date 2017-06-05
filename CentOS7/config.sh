2a2e2d0db355691987e10de27d0540622a38380d
Touhid Mia <touhid@bitmascot.com>
2017-06-05 15:39:30 +0600

    Ansible CentOS 7

--- CentOS7/config.sh
+++ CentOS7/config.sh
@@ -1,5 +1,5 @@
 echo -e "\n##################################################"
-IS_INSTALL=`rpm -qa  wget`;
+IS_INSTALL=`rpm -qa wget`;
 if [ -z "$IS_INSTALL" ]; then
     echo -e "Installing wget"
     yum -y install wget
@@ -9,10 +9,10 @@ else
 fi
 
 echo -e "\n##################################################"
-IS_INSTALL=`rpm -qa  git`;
+IS_INSTALL=`rpm -qa git`;
 if [ -z "$IS_INSTALL" ]; then
     echo -e "Installing git"
-    yum -y install wget
+    yum -y install git
     echo -e "Installing End git"
 else
     echo "Already Installed git"
@@ -20,10 +20,10 @@ fi
 
 
 echo -e "\n##################################################"
-IS_INSTALL=`rpm -qa  net-tools`;
+IS_INSTALL=`rpm -qa net-tools`;
 if [ -z "$IS_INSTALL" ]; then
     echo -e "Installing net-tools"
-    yum -y install wget
+    yum -y install net-tools
     echo -e "Installing End net-tools"
 else
     echo "Already Installed net-tools"
@@ -31,10 +31,10 @@ fi
 
 
 echo -e "\n##################################################"
-IS_INSTALL=`rpm -qa  vim`;
+IS_INSTALL=`rpm -qa vim`;
 if [ -z "$IS_INSTALL" ]; then
     echo -e "Installing vim"
-    yum -y install wget
+    yum -y install vim
     echo -e "Installing End vim"
 else
     echo "Already Installed vim"
